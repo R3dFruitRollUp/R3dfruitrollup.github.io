f70c52dd60979f5fd50eea350e3f7e76adfa02cf
Smfh <30419131+R3dFruitRollUp@users.noreply.github.com>
2023-05-02 21:03:59 +0000

    none

--- scripts.js
+++ scripts.js
@@ -1,32 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const projects = [
-      {
-        title: "Project 1",
-        description: "A responsive website for a local business.",
-        imageUrl: "https://via.placeholder.com/150",
-      },
-      {
-        title: "Project 2",
-        description: "A single-page web app using React.js.",
-        imageUrl: "https://via.placeholder.com/150",
-      },
-      {
-        title: "Project 3",
-        description: "A blog site built with Jekyll and GitHub Pages.",
-        imageUrl: "https://via.placeholder.com/150",
-      },
-    ];
-  
-    const projectsContainer = document.getElementById("projects");
-    projects.forEach((project) => {
-      const projectCard = document.createElement("div");
-      projectCard.classList.add("project-card");
-      projectCard.innerHTML = `
-        <img src="${project.imageUrl}" alt="${project.title}">
-        <h3>${project.title}</h3>
-        <p>${project.description}</p>
-      `;
-      projectsContainer.appendChild(projectCard);
-    });
+window.addEventListener('load', function () {
+  const projects = [
+    {
+      title: 'Project 1',
+      description: 'A responsive website for a local business.',
+      imageUrl: 'https://via.placeholder.com/150/0000FF/808080?Text=Placeholder',
+    },
+    {
+      title: 'Project 2',
+      description: 'A single-page web app using React.js.',
+      imageUrl: 'https://via.placeholder.com/150/0000FF/808080?Text=Placeholder',
+    },
+    {
+      title: 'Project 3',
+      description: 'A blog site built with Jekyll and GitHub Pages.',
+      imageUrl: 'https://via.placeholder.com/150/0000FF/808080?Text=Placeholder',
+    },
+  ];
+
+  const projectsContainer = document.getElementById('projects');
+  projects.forEach((project) => {
+    const projectCard = document.createElement('div');
+    projectCard.classList.add('project-card');
+    projectCard.innerHTML = `
+      <img src="${project.imageUrl}" alt="${project.title}">
+      <h3>${project.title}</h3>
+      <p>${project.description}</p>
+    `;
+    projectsContainer.appendChild(projectCard);
   });
-  
+});
