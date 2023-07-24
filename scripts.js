f182f50b66009beef48e8c307625de71df0bf4a8
Александр <30419131+R3dFruitRollUp@users.noreply.github.com>
2023-07-24 19:37:20 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,47 +1,47 @@
 $( document ).ready(function() {
   // Main variables
-  var $aboutTitle = $('.about-myself .content h2');
-  var $developmentWrapper = $('.development-wrapper');
-  var developmentIsVisible = false;
+    var $aboutTitle = $('.about-myself .content h2');
+    var $developmentWrapper = $('.development-wrapper');
+    var developmentIsVisible = false;
+
 
   /* ####### HERO SECTION ####### */
+
   $('.hero .content .header').delay(500).animate({
     'opacity':'1',
     'top': '50%'
   },1000);
 
+
   $(window).scroll( function(){
+
     var bottom_of_window = $(window).scrollTop() + $(window).height();
 
     /* ##### ABOUT MYSELF SECTION #### */
     if( bottom_of_window > ($aboutTitle.offset().top + $aboutTitle.outerHeight())){
       $('.about-myself .content h2').addClass('aboutTitleVisible');
     } 
+  /* ##### EXPERIENCE SECTION #### */
 
-    /* ##### EXPERIENCE SECTION #### */
-    $('.experience .content .hidden').each( function(i){
-        var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-        if( bottom_of_window > bottom_of_object ){
-          $(this).animate({
-            'opacity':'1',
-            'margin-left': '0'
-          },600);
-        }
-    });
+      // Check the location of each element hidden */
+      $('.experience .content .hidden').each( function(i){
 
-    /*###### PROJECTS SECTION ######*/
-    $('.projects-container .project.hidden').each( function(i){
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      if( bottom_of_window > bottom_of_object ){
-        $(this).animate({
-          'opacity':'1',
-          'margin-left': '0'
-        },600);
-      }
-    });
-    
-    /*###### SKILLS SECTION ######*/
+          var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+
+          /* If the object is completely visible in the window, fadeIn it */
+          if( bottom_of_window > bottom_of_object ){
+
+            $(this).animate({
+              'opacity':'1',
+              'margin-left': '0'
+            },600);
+          }
+      });
+
+  /*###### SKILLS SECTION ######*/
+
     var middle_of_developmentWrapper = $developmentWrapper.offset().top + $developmentWrapper.outerHeight()/2;
+
     if((bottom_of_window > middle_of_developmentWrapper)&& (developmentIsVisible == false)){
 
       $('.skills-bar-container li').each( function(){
@@ -67,4 +67,4 @@ $( document ).ready(function() {
       developmentIsVisible = true;
     }
   }); // -- End window scroll --
-}); // -- End document ready --
+});
